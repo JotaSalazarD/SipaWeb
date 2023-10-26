adefc2a279acf637eb7b03605428de07c92a5137
Jonathan Salazar <josalazar@sistepagos.com>
2023-10-25 21:42:34 -0500

    Sipa 1

--- SipaWeb/Controllers/HomeController.cs
+++ SipaWeb/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SipaWeb.Models;
 using System.Diagnostics;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace SipaWeb.Controllers
@@ -23,17 +24,79 @@ namespace SipaWeb.Controllers
 
             if (response.IsSuccessStatusCode)
             {
-                var lugaresData = await response.Content.ReadAsAsync<List<Lugares>>(); // Reemplaza "LugaresData" con el tipo de tu modelo de datos
+                var lugaresData = await response.Content.ReadAsAsync<List<Lugares>>(); 
                 return View(lugaresData);
             }
 
-            return View(); // Manejar el caso de error aquí
+            return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
+
+        public IActionResult Clientes()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> ProcesarFormulario(Clientes viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Crear una nueva instancia de la entidad Clientes y asignar valores desde el ViewModel
+                var cliente = new Clientes
+                {
+                    Ciudad = viewModel.Ciudad,
+                    Dias = viewModel.Dias,
+                    Presupuesto = viewModel.Presupuesto,
+                    Adultos =viewModel.Adultos,
+                    Ninos= viewModel.Ninos,
+                    Clima = viewModel.Clima,
+                  //  Intereses= viewModel.Intereses,
+
+                    // Asigna otras propiedades según corresponda
+                };
+
+                string[] interesesSeleccionados = viewModel.InteresesSeleccionados;
+                cliente.Intereses = string.Join(", ", interesesSeleccionados);
+
+
+                if (viewModel.Intereses != null && viewModel.Intereses.Length > 0)
+                {
+                    cliente.Intereses = Request.Form["InteresesConcatenados"];
+                }
+
+                // Realizar una solicitud POST para almacenar el cliente en el endpoint de la API
+                using (var httpClient = new HttpClient())
+                {
+                    // Especifica la URL del endpoint
+                    var apiUrl = "https://localhost:44391/api/Clientes";
+
+                    // Realiza la solicitud POST enviando el objeto cliente
+                    var response = await httpClient.PostAsJsonAsync(apiUrl, cliente);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // La solicitud fue exitosa, puedes redirigir a una vista de confirmación
+                     //   return RedirectToAction("Confirmacion");
+                    }
+                    else
+                    {
+                        // La solicitud no fue exitosa, maneja el error según sea necesario
+                        // Por ejemplo, muestra un mensaje de error al usuario
+                    }
+                }
+            }
+
+            // Si la validación falla, vuelve a mostrar el formulario con errores
+            return View(viewModel);
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ SipaWeb/Models/Clientes.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SipaWeb.Models
+{
+    public class Clientes
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int IdCliente { get; set; }
+        public string? NombreCliente { get; set; }
+        public string? Identificacion { get; set; }
+        public string? Telefono { get; set; }
+        public string? Correo { get; set; }
+        public string? Edad { get; set; }
+        public string? Genero { get; set; }
+        public string? Ciudad { get; set; }
+        public string? Dias { get; set; }
+        public string? Presupuesto { get; set; }
+        public string? Adultos { get; set; }
+        public string? Ninos { get; set; }
+        public string? Clima { get; set; }
+        public string[]? InteresesSeleccionados { get; set; }
+        public string? Intereses { get; set; }
+        public string? Q8 { get; set; }
+        public string? Q9 { get; set; }
+        public string? Q10 { get; set; }
+        public string? Descripcion { get; set; }
+
+    }
+}
